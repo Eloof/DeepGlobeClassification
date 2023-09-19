b650cbe0f96ed74343f9c285e9796e1ff2412ade
Eloof <t.vlasov23@mail.ru>
2023-09-19 12:24:45 +0300

    Update data_download.py

--- data/data_download.py
+++ data/data_download.py
@@ -40,8 +40,6 @@ class KaggleDatasetDownloader:
             logging.error(f"Error downloading dataset: {str(e)}")
 
 
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
